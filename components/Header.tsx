d7de2d53b99fe48879620ac7a97dbb2f9c0dc7b6
Justin Bartlett <justinbartlett@1.local>
2023-04-11 17:40:53 -0400

    added header pacman

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,98 @@
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '@/styles/components/Header.module.scss';
 
 export default function Header() {
+  const [pacmanDirection, setPacmanDirection] = useState('right');
+  const [pacmanPosition, setPacmanPosition] = useState(0);
   const router = useRouter();
+  const { pathname } = router;
+  const homeLinkRef = useRef<HTMLAnchorElement>(null);
+  const aboutLinkRef = useRef<HTMLAnchorElement>(null);
+  const resumeLinkRef = useRef<HTMLAnchorElement>(null);
+  const contactLinkRef = useRef<HTMLAnchorElement>(null);
+  const blogLinkRef = useRef<HTMLAnchorElement>(null);
+  let activeLinkRef = homeLinkRef;
+
+  useEffect(() => {
+    const currentPath = pathname.split('/')[1];
+    const navLinks = ['/', '/about', '/resume', '/contact', '/blog'];
+    const currentLinkIndex = navLinks.indexOf(pathname);
+    const clickedLinkIndex = navLinks.indexOf(`/${currentPath}`);
+    const pacmanClass = currentLinkIndex < clickedLinkIndex ? 'right' : 'left';
+    setPacmanDirection(pacmanClass);
+    switch (currentPath) {
+      case '':
+        activeLinkRef = homeLinkRef;
+        break;
+      case 'about':
+        activeLinkRef = aboutLinkRef;
+        break;
+      case 'resume':
+        activeLinkRef = resumeLinkRef;
+        break;
+      case 'contact':
+        activeLinkRef = contactLinkRef;
+        break;
+      case 'blog':
+        activeLinkRef = blogLinkRef;
+        break;
+      default:
+        activeLinkRef = homeLinkRef;
+        break;
+    }
+    if (activeLinkRef.current !== null) {
+      setPacmanPosition(activeLinkRef.current.offsetLeft);
+    }
+  }, [pathname]);
+
+  const currentClass = (path: string) => (path === pathname ? styles.current : '');
+  const navLinkClass = `${styles.navLink} ${currentClass('/')}`;
+  const aboutLinkClass = `${styles.navLink} ${currentClass('/about')}`;
+  const contactLinkClass = `${styles.navLink} ${currentClass('/contact')}`;
+  const resumeLinkClass = `${styles.navLink} ${currentClass('/resume')}`;
+  const blogLinkClass = `${styles.navLink} ${currentClass('/blog')}`;
+  const pacmanClass = `${styles.pacman} ${styles[`pacman${pacmanDirection}`]} ${pathname === '/' ? styles.pacmancenter : ''}`;
+  const pacmanTopClass = styles.pacmantop;
+  const pacmanBottomClass = styles.pacmanbottom;
 
   return (
     <header className={styles.header}>
-      <div className={styles.packman}>
-        <div className={styles.pacmantop}></div>
-        <div className={styles.pacmanbottom}></div>
+      <div className={pacmanClass} style={{ left: pacmanPosition }}>
+        <div className={pacmanTopClass}></div>
+        <div className={pacmanBottomClass}></div>
       </div>
-      <nav className={styles.nav}>
-        <Link href="/">
-          <span className={router.pathname === '/' ? styles.current : ''}>Home</span>
-        </Link>
-        <Link href="/about">
-          <span className={router.pathname === '/about' ? styles.current : ''}>About</span>
-        </Link>
-        <Link href="/resume">
-          <span className={router.pathname === '/resume' ? styles.current : ''}>Resume</span>
-        </Link>
-        <Link href="/contact">
-          <span className={router.pathname === '/contact' ? styles.current : ''}>Contact</span>
-        </Link>
-        <Link href="/blog">
-          <span className={router.pathname === '/blog' ? styles.current : ''}>Blog</span>
-        </Link>
+      <nav>
+        <ul className={styles.navul}>
+          <li>
+            <Link href="/">
+              <span ref={homeLinkRef} className={navLinkClass}>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span ref={aboutLinkRef} className={aboutLinkClass}>About</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/resume">
+              <span ref={resumeLinkRef} className={resumeLinkClass}>Resume</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <span ref={contactLinkRef} className={contactLinkClass}>Contact</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <span ref={blogLinkRef} className={blogLinkClass}>Blog</span>
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
+  
 }
