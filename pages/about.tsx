119953a4ac4159653368d8259d496ee1ee140691
Justin Bartlett <justinbartlett@1.local>
2023-04-11 11:19:28 -0400

    woops

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,7 +17,7 @@ Welcome to my personal website! My name is Justin, and I am a JavaScript develop
 My wide range of interests includes investing, survival skills, grilling, chess, rock climbing, guitar, marksmanship, robotics, photography, martial arts, electronic music producing, writing, and many more. I am also passionate about volunteering and coaching and mentoring others.
 </p>
 <p>
-When I'm not working or pursuing my hobbies, I enjoy spending time with my Husky puppy, Pete. I have a deep appreciation for sailboats, waterfalls, and cliff-jumping, and I love to dance, especially bboying.
+When I&apos;m not working or pursuing my hobbies, I enjoy spending time with my Husky puppy, Pete. I have a deep appreciation for sailboats, waterfalls, and cliff-jumping, and I love to dance, especially bboying.
 </p>
 <p>
 I am always striving to improve myself and expand my knowledge, and I find inspiration in figures like Marcus Aurelius and Richard Feynman. My professional background includes a transition from Navy Nuke to Designer to Dev to DevOps and ML, quantitative finance, and I am currently Founder of Floorism and Grit & Merit.
