4bc26af80dc623a59d551ae6cbf87c53b920f257
Justin Bartlett <justinbartlett@1.local>
2023-04-15 15:56:41 -0400

    page transition

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,29 +1,40 @@
-import React, { forwardRef, useMemo } from 'react'
+import React, { forwardRef, useMemo, useEffect, useState } from 'react'
 import { motion, HTMLMotionProps } from 'framer-motion'
 
 type PageTransitionProps = HTMLMotionProps<'div'>
 type PageTransitionRef = React.ForwardedRef<HTMLDivElement>
 
 function PageTransition({ children, ...rest }: PageTransitionProps, ref: PageTransitionRef) {
-	const onTheRight = { x: '-100%' }
-	const inTheCenter = { x: 0 }
-	const onTheLeft = { x: '100%' }
-
-	const transition = { duration: 0.6, ease: 'easeInOut' }
-
+	const [isExiting, setIsExiting] = useState(false);
+  
+	const onTheRight = { y: '-100%' };
+	const inTheCenter = { y: 0 };
+	const onTheLeft = { y: '100%' };
+  
+	const transition = { duration: 0.6, ease: 'easeInOut' };
+  
+	const handleExitComplete = () => setIsExiting(false);
+  
+	useEffect(() => {
+	  if (isExiting) {
+		setTimeout(handleExitComplete, transition.duration * 1000);
+	  }
+	}, [isExiting]);
+  
 	return (
-		<motion.div
-			ref={ref}
-			initial={onTheRight}
-			animate={inTheCenter}
-			exit={onTheLeft}
-			transition={transition}
-			style={{ willChange: 'transform' }}
-			{...rest}
-		>
-			{children}
-		</motion.div>
-	)
-}
+	  <motion.div
+		ref={ref}
+		initial={onTheRight}
+		animate={inTheCenter}
+		exit={onTheLeft}
+		transition={transition}
+		onAnimationStart={() => setIsExiting(true)}
+		onAnimationEnd={handleExitComplete}
+		{...rest}
+	  >
+		{children}
+	  </motion.div>
+	);
+  }
 
 export default forwardRef(PageTransition)
