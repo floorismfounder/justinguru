536edaca8fa7af6b9f9621dd227add2f5a0c0f54
Justin Bartlett <justinbartlett@1.local>
2023-04-16 18:10:57 -0400

    maybe

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,44 +1,41 @@
-import React, { forwardRef, useMemo, useEffect, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import { motion, HTMLMotionProps } from 'framer-motion'
 import { useRouter } from 'next/router';
-
 
 type PageTransitionProps = HTMLMotionProps<'div'>
 type PageTransitionRef = React.ForwardedRef<HTMLDivElement>
 
 function PageTransition({ children, ...rest }: PageTransitionProps, ref: PageTransitionRef) {
-	const [isExiting, setIsExiting] = useState(false);
-  
-	// const router = useRouter();
-	const onTheRight = { x: '100%' };
-	const inTheCenter = { x: 0 };
-	const onTheLeft = { x: '-100%'};
-  
-	const transition = { duration: 0.3, ease: 'easeInOut' };
-  
-	const handleExitComplete = () => setIsExiting(false);
-  
-	useEffect(() => {
-	  if (isExiting) {
-		setTimeout(handleExitComplete, transition.duration * 1000);
-	  }
-	}, [isExiting]);
-  
-	return (
-	  <motion.div
-		// key={router.route}
-		ref={ref}
-		initial={onTheRight}
-		animate={inTheCenter}
-		exit={onTheLeft}
-		transition={transition}
-		onAnimationStart={() => setIsExiting(true)}
-		onAnimationEnd={handleExitComplete}
-		{...rest}
-	  >
-		{children}
-	  </motion.div>
-	);
-  }
+  const [isExiting, setIsExiting] = useState(false);
+
+  const onTheRight = { x: '100%' };
+  const inTheCenter = { x: 0 };
+  const onTheLeft = { x: '-100%'};
+
+  const transition = { duration: 0.4, ease: 'easeInOut' };
+
+  const handleExitComplete = () => setIsExiting(false);
+
+  useEffect(() => {
+    if (isExiting) {
+      setTimeout(handleExitComplete, transition.duration * 1000);
+    }
+  }, [isExiting]);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={onTheRight}
+      animate={inTheCenter}
+      exit={onTheLeft}
+      transition={transition}
+      onAnimationStart={() => setIsExiting(true)}
+      onAnimationEnd={handleExitComplete}
+      {...rest}
+    >
+      {children}
+    </motion.div>
+  );
+}
 
 export default forwardRef(PageTransition)
